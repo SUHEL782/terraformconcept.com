620f28d2d2e4380abfe45deda695372b1e9d005a
suhel <thecomputersk@gamil.com>
2025-04-12 19:20:23 +0530

    done

--- s3.tf
+++ s3.tf
@@ -2,6 +2,9 @@ resource "aws_s3_bucket" "my_s3_bucket" {
     bucket = "my-unique-bucket-name"
     acl    = "private"
     
+    force_destroy = true
+    
+
     versioning {
         enabled = true
     }
